255c0bdc6f80142aaa6cc0e3049189e76bf5306e
Brian Hines <brian@projectweekend.net>
2015-11-25 20:17:23 -0600

    Version bump

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='PiCloud-Client',
-    version='1.0.0',
+    version='2.0.0',
     author='Brian Hines',
     author_email='brian@projectweekend.net',
     packages=['picloud_client'],
